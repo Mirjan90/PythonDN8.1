6ee0c6a91fb31d0610210c65d681ee858a54ce29
Mirjan M <mirjan.miladic@gmail.com>
2020-09-15 13:56:16 +0200

    DN08.01

---
+++ main.py
@@ -0,0 +1,16 @@
+mood = input("Enter mood (Happy, Nervous, Sad, Relaxed, aything else?): ")
+
+if mood == "happy":
+    print("It is great to see you happy!")
+
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+
+elif mood == "sad":
+    print("Try to think something happy, no need to be sad :).")
+
+elif mood == "relaxed":
+    print("That is great! Just enjoy the day!")
+
+else:
+    print("I don't recognize this mood.")
